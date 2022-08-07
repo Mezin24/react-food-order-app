760a35c8e9b51a3cf1ad7e1ed164927f09205a36
Pavel Mezencev <mezencev24@mail.ru>
2022-08-07 16:50:28 +0300

    change meal component

--- src/components/Meals/Meal.js
+++ src/components/Meals/Meal.js
@@ -1,11 +1,15 @@
 import classes from './Meal.module.css';
 
 const Meal = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
   return (
     <li className={classes.meal}>
-      <h3>{props.name}</h3>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>€ {props.price}</div>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
     </li>
   );
 };
